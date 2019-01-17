87060f776fc3e4c505af4f3e31ccc32d69ad5069
tangxin <tangxin@tangxin-mbp.local>
2019-01-17 14:30:29 +0800

    add confd

---
+++ README.md
@@ -0,0 +1,5 @@
+# docker-handbook
+
+## issus cellection
+
++ [Driver devicemapper failed to remove root filesystem. Device is busy](https://github.com/moby/moby/issues/27381)
--- confd/start.sh
+++ confd/start.sh
@@ -6,8 +6,8 @@ cd $(dirname $0)
 confdir=$(pwd)
 
 ## file
-# confd -confdir $confdir  -interval 1 -backend file -file yaml/myapp-nginx.yaml
+confd -confdir $confdir  -interval 1 -backend file -file yaml/nginx.yaml
 
 ## redis
-confd -confdir $confdir  -interval 1 -backend redis -node ${redis_host_string}:${redis_port_int}/${db_int} -client-key p4FUvg9wm
+# confd -confdir $confdir  -interval 1 -backend redis -node ${redis_host_string}:${redis_port_int}/${db_int} -client-key p4FUvg9wm
 
